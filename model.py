92e949878957e5180398278f220bd3d942753ce3
kimsehwan96 <sktpghks138@gmail.com>
2022-12-02 23:20:51 +0900

    WIP

---
+++ model.py
@@ -0,0 +1,34 @@
+import dataclasses
+from datetime import date
+from typing import Optional
+@dataclasses.dataclass(frozen=True)
+class OrderLine:
+    orderId: str
+    sku: str
+    quantity: int
+
+class Batch:
+    def __init__(
+        self,
+        reference: str,
+        sku: str,
+        available_quantity: int,
+        eta: Optional[date] = None
+    ) -> None:
+        self.reference = reference
+        self.sku = sku
+        self.available_quantity = available_quantity
+        self.eta = eta
+    
+    def _can_allocate(self, orderLine) -> bool: 
+        return orderLine.sku == self.sku and self.available_quantity >= orderLine.quantity
+
+    def allocate(self, orderLine: OrderLine) -> None:
+        if orderLine.sku is not self.sku:
+            raise RuntimeError(
+                f"You can't allocate {orderLine.orderId} OrderLine in \
+                {self.reference} Batch because each other's sku isn't same \
+                Orderline sku : {orderLine.sku}\n \
+                Batch sku : {self.sku}")
+        if self._can_allocate(orderLine=orderLine):
+            self.available_quantity -= orderLine.quantity
